1384351c6d5ec9e1568130131f9f113d77cdc894
sushmabhapkar <sushmabhapkar244@gmail.com>
2025-06-16 20:45:16 +0530

    CORS ISSUE UPDATED CODE

--- app.js
+++ app.js
@@ -15,24 +15,10 @@ app.use(express.json());
 
 
 // Middleware for handling CORS
-const allowedOrigins = [
-  'https://advikaadmin.vercel.app',
-  'https://advikafront.vercel.app'
-];
-
-// ✅ CORS configuration
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, curl, etc.)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-}));
+    origin: true
+}))
+
 
 // Use morgan for logging HTTP requests (this is very useful in production)
 app.use(morgan('combined'));
