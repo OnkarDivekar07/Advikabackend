503bebbb5fa4c73def7df23fbc0b8b57a6461b2f
sushmabhapkar <sushmabhapkar244@gmail.com>
2025-06-26 21:30:38 +0530

    admin stats api updated

--- services/Admin/adminService.js
+++ services/Admin/adminService.js
@@ -2,14 +2,28 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 exports.getAdminStats = async () => {
-  const totalUsers = await prisma.user.count();
-  const totalOrders = await prisma.order.count();
-  const totalProducts = await prisma.product.count();
-  const deliveredOrders = await prisma.order.count({ where: { status: 'delivered' } });
-  const pendingOrders = await prisma.order.count({ where: { status: 'pending' } });
+  const totalUsers = await prisma.user.count({
+    where: { role: 'customer' }
+  });
 
-  
+  const totalOrders = await prisma.order.count({
+    where: { status: 'confirmed' }
+  });
+
+  const totalProducts = await prisma.product.count({
+    where: { isDeleted: false }
+  });
+
+  const deliveredOrders = await prisma.order.count({
+    where: { status: 'delivered' }
+  });
+
+  const pendingOrders = await prisma.order.count({
+    where: { status: 'pending' }
+  });
+
   const totalRevenueResult = await prisma.order.aggregate({
+    where: { paymentStatus: 'paid' },
     _sum: { total: true }
   });
 
